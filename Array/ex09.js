d5e0a5dbbb060b736a76507b6f3ee020ccda1655
Pedro U <pedroescapalete@gmail.com>
2025-04-25 11:19:10 -0300

    ex09

--- Array/ex09.js
+++ Array/ex09.js
@@ -1,4 +1,5 @@
 let frutas = ['maça', 'banana', 'uva', 'melao', 'kiwi', 'manga'];
 let quantidade = frutas.length;
 
+for(i = 5; i < quantidade; i++)
 console.log(`Tem no total ${quantidade} frutas`);
