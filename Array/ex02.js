59dc972bc3c57a34277fa96c03c7741d88aba1ca
Pedro U <pedroescapalete@gmail.com>
2025-04-25 09:47:53 -0300

    ex02

--- Array/ex02.js
+++ Array/ex02.js
@@ -1,4 +1,4 @@
-let playlist = ['Black', 'Careless Whisper', 'Estilo Cachorro', 'Purple Haze'];
+let playlist = ['Black', 'Careless Whisper', 'Killer Queen', 'Purple Haze'];
 let quantidade = playlist.length;
 for(let i = 0; i < quantidade; i ++){
     console.log(playlist[i]);
